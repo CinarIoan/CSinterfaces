e6536aefeac17e3b1cd602d8ee64d8fec588f60d
CinarIoan <cinar.ioan@gmail.com>
2020-12-05 11:28:08 +0200

    added the first files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csinterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
